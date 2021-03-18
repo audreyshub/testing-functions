64ffe7f269bef2f607d1ab56d9df9b8a40cd528d
Audrey Wong <audreydotwong@gmail.com>
2021-03-18 15:46:17 -0400

    edit function

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
-exports.handler = (event, context, callback) => {
-    callback(null, {
+exports.handler = async function(event, context) {
+    return {
         statusCode: 200,
-        headers,
-        body: 'Yey!'
-    });
-};
+        headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+      },
+        body: "Hello World"
+    };
+}
